db9850bc29f2c0ff5793ab136a1c595590f2814d
hsh <74010090+dev-hsonghart@users.noreply.github.com>
2026-05-14 19:08:16 +0900

    fix/ 편지 생성 시 오늘 날짜 iso형식으로 전달

--- src/pages/ShareLink.tsx
+++ src/pages/ShareLink.tsx
@@ -14,11 +14,7 @@ export default function ShareLink() {
   const to = state?.to;
   const from = state?.from;
   const content = state?.content;
-  const now = new Date();
-  const date = `${now.getFullYear()}년 ${String(now.getMonth() + 1).padStart(
-    2,
-    "0"
-  )}월 ${String(now.getDate()).padStart(2, "0")}일`;
+  const date = new Date().toLocaleDateString();
   const id = state?.id;
 
   const letterPassword = state?.letterPassword ?? "";
