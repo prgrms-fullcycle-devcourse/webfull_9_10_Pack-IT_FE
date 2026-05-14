2505bc4bae9d65217b9b9104de93c3187cd03642
WookHub <scw0132@gmail.com>
2026-05-14 18:51:33 +0900

    fix/vite.config.ts에 loadEnv 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api/proxy': {
-        target: 'http://54.180.159.211:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/proxy/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/api/proxy': {
+          target: env.VITE_PROXY_TARGET, 
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api\/proxy/, ''),
+        },
       },
     },
-  },
+  }
 })
